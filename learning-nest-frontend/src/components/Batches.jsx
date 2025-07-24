e7697c801917c8f260a4643cab74d8af713ecd97
dipikasahoo <dipikasahoo004@gmail.com>
2025-07-24 16:23:03 +0530

    batches updated

--- learning-nest-frontend/src/components/Batches.jsx
+++ learning-nest-frontend/src/components/Batches.jsx
@@ -1,18 +1,23 @@
 const Batches = () => {
   return (
     <section className="w-full bg-[#F5E9DC] py-16 px-6 md:px-12 lg:px-24 font-sans">
-      <h2 id="batches" className="text-4xl md:text-5xl font-bold text-[#583015] text-center mb-10">
+      <h2
+        id="batches"
+        className="text-4xl md:text-5xl font-bold text-[#583015] text-center mb-10"
+      >
         Batch Timings
       </h2>
 
-      <div 
-      
-      className="max-w-3xl mx-auto overflow-x-auto shadow-lg border border-[#A27448] rounded-3xl">
+      <div className="max-w-3xl mx-auto overflow-x-auto shadow-lg border border-[#A27448] rounded-3xl">
         <table className="min-w-full table-auto bg-white">
           <thead>
             <tr className="bg-[#A27448] text-white">
-              <th className="py-4 px-12 text-lg font-semibold text-left rounded-tl-xl">Batch</th>
-              <th className="py-4 px-20 text-lg font-semibold text-right rounded-tr-xl">Timing</th>
+              <th className="py-4 px-6 sm:px-12 text-lg font-semibold text-left rounded-tl-xl whitespace-nowrap">
+                Batch
+              </th>
+              <th className="py-4 px-6 sm:px-12 text-lg font-semibold text-right rounded-tr-xl whitespace-nowrap">
+                Timing
+              </th>
             </tr>
           </thead>
           <tbody className="text-[#8C745C] text-md">
@@ -25,19 +30,17 @@ const Batches = () => {
             ].map(([batch, timing], idx, arr) => (
               <tr
                 key={batch}
-                className={`border-b border-[#E3D3BC] hover:bg-[#f9f4ef] transition ${
-                  idx === arr.length - 1 ? "" : "border-b"
-                }`}
+                className={`border-b border-[#E3D3BC] hover:bg-[#f9f4ef] transition`}
               >
                 <td
-                  className={`py-3 px-12 text-left ${
+                  className={`py-3 px-6 sm:px-12 text-left whitespace-nowrap ${
                     idx === arr.length - 1 ? "rounded-bl-xl" : ""
                   }`}
                 >
                   {batch}
                 </td>
                 <td
-                  className={`py-3 px-14 text-right ${
+                  className={`py-3 px-6 sm:px-12 text-right whitespace-nowrap ${
                     idx === arr.length - 1 ? "rounded-br-xl" : ""
                   }`}
                 >
